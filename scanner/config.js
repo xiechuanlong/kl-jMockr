a78d58851ba1943d5bb05dd387f2894045d2be96
xiechuanlong <xiechuanlong666@163.com>
2019-07-08 20:18:27 +0800

    misc: fixed

--- scanner/config.js
+++ scanner/config.js
@@ -1,8 +1,14 @@
 const path = require('path');
 const fs = require('fs');
 const fileUtil = require('../util/fileUtil');
+const { isUndefined} = require('../util/typeUtil');
 let config = fileUtil.json5Require(path.resolve('./jmockr.config.json'));
 const envData = JSON.parse((fs.readFileSync('./.env')).toString())
-config.authConfig.entranceEnv = config.authConfig.entranceEnv || envData.entranceEnv
-config.proxyConfig.enable = config.proxyConfig.enable || envData.isProxy
+if(isUndefined(config.authConfig.entranceEnv)) {
+    config.authConfig.entranceEnv = envData.entranceEnv
+}
+if(isUndefined(config.proxyConfig.enable )) {
+    config.proxyConfig.enable = config.proxyConfig.enable || envData.isProxy
+}
+console.log(config, 5555555555555555)
 module.exports = config;
